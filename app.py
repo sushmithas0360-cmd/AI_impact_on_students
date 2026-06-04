46d1620af1c63042bddf8d1c1a7da6e0f03dbcea
Sushmithagowda <sushmithas0360@gmail.com>
2026-06-04 15:37:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # Load Dataset
-df = pd.read_csv("ai_student_impact_dataset.csv")
+df = pd.read_csv("ai_student_impact_dataset (1).csv")
 
 # Title
 st.title("🎓 AI Student Impact Analysis Dashboard")
